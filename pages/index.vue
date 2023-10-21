4cd8777d101f8a741e2d7910428aed6c35f77a82
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-10-21 17:30:37 +0500

    home pages ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,48 @@
 <template>
   <div>
     <div class="conatainer">
-      <header class="home" >
-        <Navbar/>
+      <header class="home w-[100%] h-[120vh] bg-[#FCF7F7]">
+        <Navbar />
+        <div class="main-content w-[100%] h-[500px] mt-[70px] flex justify-center">
+          <div class="w-[93.45%] h-[100%] flex justify-between items-end">
+            <div class="w-[650px] h-[100%] ">
+              <div class="w-[100%] h-[auto] text-black text-[80px] font-bold font-['Roboto Condensed']">
+                Muhammadamin</div>
+              <div class="w-[100%] h-[71px] mt-[40px]"><span
+                  class="text-black text-6xl font-normal font-['Roboto Condensed'] "> {</span><span
+                  class="text-cyan-400 text-6xl font-bold font-['Roboto Condensed'] ">Front-End}
+                </span><span class="text-black text-6xl font-normal font-['Roboto Condensed'] ">Developer</span>
+              </div>
+              <div class="w-[100%] v-[100%] flex justify-between">
+                <button class="w-[150px] h-[52.55px] mt-[40px] rounded-[10.20px]"
+                  style="background: linear-gradient(106deg, rgba(255, 255, 255, 0.21) 40.3%, rgba(15, 45, 207, 0.12) 58.72%), linear-gradient(180deg, rgba(145, 17, 205, 0.35) 0%, rgba(0, 209, 255, 0.47) 100%)">
+                  <div class="w-[150px] h-[100%] flex justify-center items-center  ">
+                    <div class="w-[150px] h-[38px] text-neutral-800 text-2xl font-medium font-['Roboto'] leading-normal">
+                      &lt Hire me />
+                    </div>
+                  </div>
+                </button>
+                <div class="w-[463.456px] h-[300.467px]">
+                  <svg class="w-[100%]" src="../assets/images/Ale-icons.svg" alt="#">
+  
+                  </svg>
+                </div>
+              </div>
+
+
+            </div>
+
+            <div class="w-[650px] h-[100%] bg-[red]"></div>
+
+          </div>
+
+        </div>
       </header>
     </div>
   </div>
 </template>
 
 <script  setup>
-
-
-
 
 </script>
 
@@ -23,20 +54,4 @@
   max-width: 100%;
   height: auto;
 }
-
-.home {
-  width: 100%;
-  height: 100vh;
-  max-height: 100%;
-  background-color: #FDF8F8;
-  display: flex;
-  justify-content: center;
-}
-.input{
-  width: 100px;
-  height: 30px;
-  background-color: #000;
-  color: #fff;
-}
-
 </style>

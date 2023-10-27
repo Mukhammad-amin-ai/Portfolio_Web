c76d5e52a839746779842f3fe177e15da5a301f9
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-10-27 16:27:43 +0500

    fully completed

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,58 @@
 <template >
-    <div class="w-[100%] h-[100%]">
-        
+    <div class="w-[100%] h-[100%] flex justify-center">
+        <div class="w-[90%] h-[100%] flex justify-between items-end">
+            <div class="w-[400px] h-[auto]">
+                <div
+                    class="w-[127.02px] h-[auto] text-black text-[49.51px] font-semibold font-['Roboto'] leading-[50.84px]">
+                    LOGO</div>
+                <div class="w-[100%] h-[aut] text-black text-[31.87px] font-medium font-['Roboto']">Designing Tomorrow's
+                    Solutions Today.</div>
+                <div class="w-[100%] h-[auto] text-black text-[22.52px] font-light font-['Roboto']">Copyright reserved 2023
+                </div>
+                <div class="w-[100%] h-[65px] flex gap-[26px] mt-[30px]">
+                    <div class="w-[65px] h-[100%]">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 [width] [height]">
+                            <image href="../assets/svg/instagram.svg" width="100%" height="100%" />
+                        </svg>
+                    </div>
+                    <div class="w-[65px] h-[100%]">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 [width] [height]">
+                            <image href="../assets/svg/linkedin.svg" width="100%" height="100%" />
+                        </svg>
+                    </div>
+                    <div class="w-[65px] h-[100%]">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 [width] [height]">
+                            <image href="../assets/svg/github.svg" width="100%" height="100%" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div class="w-[150px] h-[auto]">
+                <div class="w-[148px] h-12 text-black text-[39.64px] font-medium font-['Roboto']">Menu</div>
+                <div class="mt-[27px] w-[136px] h-[200px] text-black text-lg font-normal font-['Roboto'] ">
+                    <ul class="w-[100%] h-[100%] flex flex-col justify-between">
+                        <li>
+                            Home
+                        </li>
+                        <li>
+                            Portfolio
+                        </li>
+                        <li>
+                            Resume
+                        </li>
+                        <li>
+                            About me
+                        </li>
+                        <li>
+                            Contacts
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
 
 </script>
-<style scoped>
-    
-</style>
+<style scoped></style>

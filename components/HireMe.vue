fcfebe1eb92e2250061e21b1a5bd3f0f89936d20
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-10-27 15:21:50 +0500

    some changes

--- components/HireMe.vue
+++ components/HireMe.vue
@@ -1,16 +1,23 @@
 <template >
     <div class="w-[100%] h-[100vh] mt-[100px] flex justify-center items-center">
         <div class="w-[90%] h-[90%] flex items-center flex-col">
-            <div class="w-[303px] h-[98px] text-black text-[82.64px] font-semibold font-['Roboto'] leading-[84.86px]">Hire Me</div>
-            <div class="">
+            <div class="w-[303px] h-[98px] text-black text-[82.64px] font-semibold font-['Roboto'] leading-[84.86px]">Hire
+                Me</div>
+            <div class="w-[893px] h-[614px] bg-black rounded-[27.02px] border-2 border-emerald-500 mt-[31px]">
+                <div class="w-[100%] h-[60px]  border-b-2 border-emerald-500">
+                    <div class="w-[100px] h-[16px] mt-[30px] ml-[10px]">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 [width] [height]">
+                            <image href="../assets/svg/icons.svg" width="100%" height="100%" />
+                        </svg>
+                    </div>
+                </div>
+
 
             </div>
-        </div>    
+        </div>
     </div>
 </template>
 <script setup>
 
 </script>
-<style scoped>
-    
-</style>
+<style scoped></style>

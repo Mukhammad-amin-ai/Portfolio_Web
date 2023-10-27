bf0981268500d4e1aead2a5b3982c03390cd8d0b
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-10-27 14:44:29 +0500

    hire me

--- components/HireMe.vue
+++ components/HireMe.vue
@@ -1,6 +1,11 @@
 <template >
-    <div class="w-[100%] h-[100vh]">
-        
+    <div class="w-[100%] h-[100vh] mt-[100px] flex justify-center items-center">
+        <div class="w-[90%] h-[90%] flex items-center flex-col">
+            <div class="w-[303px] h-[98px] text-black text-[82.64px] font-semibold font-['Roboto'] leading-[84.86px]">Hire Me</div>
+            <div class="">
+
+            </div>
+        </div>    
     </div>
 </template>
 <script setup>

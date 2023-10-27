ea76f4fbdd5ac39f7db381ada4b3cb4b97f51a8f
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-10-27 14:36:08 +0500

    about me section

--- components/Aboutme.vue
+++ components/Aboutme.vue
@@ -1,12 +1,30 @@
 <template >
-    <div class="w-[100%] h-[100vh] ">
-        
+    <div class="w-[100%] h-[100vh]  flex justify-center mt-[10vh] ">
+        <div class="w-[90%] h-[100%] flex justify-between">
+            <div class="w-[40%] h-[100%] flex flex-col justify-center items-center gap-[50px]">
+                <div class="w-[100%] h-[auto] text-black text-[60px] font-semibold font-['Roboto'] leading-[83.28px]">
+                    About me</div>
+                <div class="w-[100%] h-[auto]"><span
+                        class="text-black text-[35px] font-light font-['Roboto'] ">Hello , my name is
+                    </span><span
+                        class="text-black text-[35px] font-bold font-['Roboto'] ">Muhammadamin</span><span
+                        class="text-black text-[35px] font-light font-['Roboto'] "> 👋.
+                        <br /></span><span
+                        class="text-black text-[35px] font-bold font-['Roboto'] "><br /></span><span
+                        class="text-black text-[35px] font-light font-['Roboto'] ">I'm a dedicated
+                        front-end engineer that is driven to create amazing user experiences. Having worked in the industry
+                        for a year, I've refined my HTML, CSS, and JavaScript abilities, which enable me to turn design
+                        conceptions into fully functional, responsive websites and web applications.</span></div>
+            </div>
+
+            <div class=" w-[50%] h-[100%] flex items-center justify-center">
+                <img class="w-[70%] h-[60%]" src="../assets/svg/foto-ale.png" alt="#">
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
 
 
 </script>
-<style scoped>
-    
-</style>
+<style scoped></style>

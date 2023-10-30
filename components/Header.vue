c8ad07ac23616401670c5feefaf620d8f233b473
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-10-30 14:43:32 +0500

    changes

--- components/Header.vue
+++ components/Header.vue
@@ -11,15 +11,17 @@
                         </span><span class="text-black text-6xl font-normal font-['Roboto Condensed'] ">Developer</span>
                     </div>
                     <div class="w-[100%] v-[100%] flex justify-between">
-                        <button class="w-[150px] h-[52.55px] mt-[40px] rounded-[10.20px]"
-                            style="background: linear-gradient(106deg, rgba(255, 255, 255, 0.21) 40.3%, rgba(15, 45, 207, 0.12) 58.72%), linear-gradient(180deg, rgba(145, 17, 205, 0.35) 0%, rgba(0, 209, 255, 0.47) 100%)">
-                            <div class="w-[150px] h-[100%] flex justify-center items-center  ">
-                                <div
-                                    class="w-[150px] h-[38px] text-neutral-800 text-2xl font-medium font-['Roboto'] leading-normal">
-                                    &lt Hire me />
+                        <nuxt-link to="#hearMe">
+                            <button class="w-[150px] h-[52.55px] mt-[40px] rounded-[10.20px]"
+                                style="background: linear-gradient(106deg, rgba(255, 255, 255, 0.21) 40.3%, rgba(15, 45, 207, 0.12) 58.72%), linear-gradient(180deg, rgba(145, 17, 205, 0.35) 0%, rgba(0, 209, 255, 0.47) 100%)">
+                                <div class="w-[150px] h-[100%] flex justify-center items-center  ">
+                                    <div
+                                        class="w-[150px] h-[38px] text-neutral-800 text-2xl font-medium font-['Roboto'] leading-normal">
+                                        &lt Hire me />
+                                    </div>
                                 </div>
-                            </div>
-                        </button>
+                            </button>
+                        </nuxt-link>
                         <div class="w-[463.456px] h-[300.467px]">
                             <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"
                                 viewBox="0 0 [width] [height]">
@@ -30,8 +32,7 @@
 
                 </div>
                 <div class="mac-box w-[650px] h-[100%] ">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0  "
-                        class="relative">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0  " class="relative">
                         <image href="../assets/svg/Bloco-glass.svg" width="100%" height="100%" />
                     </svg>
                 </div>
@@ -44,8 +45,6 @@
 
 </script>
 <style scoped>
-
-
 .box-header {
     width: 100%;
     height: 92px;
--- components/HireMe.vue
+++ components/HireMe.vue
@@ -1,5 +1,5 @@
 <template >
-    <div class="w-[100%] h-[100vh] mt-[100px] flex justify-center items-center">
+    <div id="hearMe" class="w-[100%] h-[100vh] mt-[100px] flex justify-center items-center">
         <div class="w-[90%] h-[90%] flex items-center flex-col">
             <div class="w-[303px] h-[98px] text-black text-[82.64px] font-semibold font-['Roboto'] leading-[84.86px]">Hire
                 Me</div>
